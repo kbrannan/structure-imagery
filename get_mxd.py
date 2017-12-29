df86a06cac82f9adf992cef2b08b0f8a0c91fb5b
kbrannan <brannan.kevin@deq.state.or.us>
2017-12-29 13:55:42 -0800

    put impory arcpy and os on smae line. no progress on getting the mxd. not returning the object yet

--- get_mxd.py
+++ get_mxd.py
@@ -1,5 +1,4 @@
-import arcpy
-import os.path
+import arcpy, os
 
 str_path_mxd = r'M:\GIS\BacteriaTMDL\UpperYaquinaRiver\MapDocs'
 str_file_mxd = r'compare-near-streamstructures-locations-to-imagery-template20171227.mxd'
