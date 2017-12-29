eb7cb846083881401219faac859eeb31d7be73e2
kbrannan <brannan.kevin@deq.state.or.us>
2017-12-29 13:17:46 -0800

    still trying to get the mxd. not returning the object yet

--- get_mxd.py
+++ get_mxd.py
@@ -1,4 +1,21 @@
 import arcpy
+import os.path
 
 str_path_mxd = r'M:\GIS\BacteriaTMDL\UpperYaquinaRiver\MapDocs'
 str_file_mxd = r'compare-near-streamstructures-locations-to-imagery-template20171227.mxd'
+
+if os.path.isfile(str_path_mxd + "\\" + str_file_mxd):
+    mxd_cur = arcpy.mapping.MapDocument(str_path_mxd + "\\" + str_file_mxd)
+    df = arcpy.mapping.ListDataFrames(mxd_cur)
+
+
+
+for df in arcpy.mapping.ListDataFrames(mxd_cur):
+    print df.name
+
+
+x = 8
+if x==10:
+    print('X is 10')
+else:
+    print('Not')
