1ee91c75eb308bba9343614fa35c66016f8073b4
kbrannan <brannan.kevin@deq.state.or.us>
2018-01-03 14:19:21 -0800

    fixed problem of not getting mxd by using the absolute network path rather than the mapped network drive and fixed data frmae object get by including the index [0]

--- get_mxd.py
+++ get_mxd.py
@@ -1,16 +1,17 @@
 import arcpy, os
 
-str_path_mxd = r'M:\GIS\BacteriaTMDL\UpperYaquinaRiver\MapDocs'
+str_path_mxd = r'\\deqhq1\tmdl\tmdl_wr\midcoast\GIS\BacteriaTMDL\UpperYaquinaRiver\MapDocs'
 str_file_mxd = r'compare-near-streamstructures-locations-to-imagery-template20171227.mxd'
+
 
 if os.path.isfile(str_path_mxd + "\\" + str_file_mxd):
     mxd_cur = arcpy.mapping.MapDocument(str_path_mxd + "\\" + str_file_mxd)
-    df = arcpy.mapping.ListDataFrames(mxd_cur)
+    df = arcpy.mapping.ListDataFrames(mxd_cur)[0]
 
 
 
-for df in arcpy.mapping.ListDataFrames(mxd_cur):
-    print df.name
+for lyr in df:
+    print lyr.name
 
 
 x = 8
