8628dee2ef5727268a1e9a50d2f32ee08470394f
kbrannan <brannan.kevin@deq.state.or.us>
2018-01-03 16:39:21 -0800

    got layer and now working on zooming to a feterure in the PointPotentialStructureCentroids layer

--- get_mxd.py
+++ get_mxd.py
@@ -2,20 +2,16 @@ import arcpy, os
 
 str_path_mxd = r'\\deqhq1\tmdl\tmdl_wr\midcoast\GIS\BacteriaTMDL\UpperYaquinaRiver\MapDocs'
 str_file_mxd = r'compare-near-streamstructures-locations-to-imagery-template20171227.mxd'
-
+str_strc_cent = r'PointPotentialStructureCentroids'
 
 if os.path.isfile(str_path_mxd + "\\" + str_file_mxd):
     mxd_cur = arcpy.mapping.MapDocument(str_path_mxd + "\\" + str_file_mxd)
     df = arcpy.mapping.ListDataFrames(mxd_cur)[0]
 
-
-
 for lyr in df:
     print lyr.name
+del(lyr)
 
 
-x = 8
-if x==10:
-    print('X is 10')
-else:
-    print('Not')
+lyr_strc_cent = arcpy.mapping.Layer(mxd_cur, str_strc_cent, df)[0]
+print arcpy.GetCount_management(lyr_strc_cent)
