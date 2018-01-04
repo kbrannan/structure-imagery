f2fd10c383b2f67440a000fe8afaeb22b851cc2b
kbrannan <brannan.kevin@deq.state.or.us>
2018-01-04 12:31:15 -0800

    cleaning up file

--- get_mxd.py
+++ get_mxd.py
@@ -15,3 +15,11 @@ del(lyr)
 
 lyr_strc_cent = arcpy.mapping.Layer(mxd_cur, str_strc_cent, df)[0]
 print arcpy.GetCount_management(lyr_strc_cent)
+desc = arcpy.Describe(lyr_strc_cent)
+
+print lyr_strc_cent
+
+fieldList = arcpy.ListFields(mxd_cur, str_strc_cent)
+
+
+flds = [f.name for f in arcpy.ListFields(lyr_strc_cent)]
