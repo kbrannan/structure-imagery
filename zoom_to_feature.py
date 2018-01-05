9e2a8b7222e6e98e297aaf27c3ad77c16980a2ea
kbrannan <brannan.kevin@deq.state.or.us>
2018-01-04 16:50:32 -0800

    got it to zoom to a location and export to imange file

--- zoom_to_feature.py
+++ zoom_to_feature.py
@@ -11,14 +11,19 @@ if os.path.isfile(str_path_mxd + "\\" + str_file_mxd):
     mxd_cur = arcpy.mapping.MapDocument(str_path_mxd + "\\" + str_file_mxd)
     df = arcpy.mapping.ListDataFrames(mxd_cur)[0]
 
-SelLayer = arcpy.mapping.ListLayers(mxd, str_strc_cent, df)[0]
+SelLayer = arcpy.mapping.ListLayers(mxd_cur, str_strc_cent, df)[0]
 
-with arcpy.SearchCursor(SelLayer, ['OID@']) as cursor:
-    for row in cursor:
-        df.extent = row.SHAPE.extent #Set the dataframe extent to the extent of the feature
-        df.scale = df.scale * 1.07 #Optionally give the shape a bit of padding around the edges
-        arcpy.RefreshActiveView()
-        arcpy.mapping.ExportToPNG(mxd_cur, str_path_export + str_file_image_export_prefix + '{}'.format(row[0]) + 'png',
+
+desc = arcpy.Describe(SelLayer)
+fields = desc.fields
+for field in fields:
+    print field.aliasName
+
+query = '"FID" = {}'.format(myList[0])
+
+arcpy.SelectLayerByAttribute_management(SelLayer, 'NEW_SELECTION', query)
+df.zoomToSelectedFeatures()
+arcpy.RefreshActiveView()
+arcpy.mapping.ExportToPNG(mxd_cur, str_path_export + '\\' + str_file_image_export_prefix + '{}'.format(myList[0]) + '.png',
                                   df, df_export_width=1600, df_export_height=1600, world_file=True)
-
 
