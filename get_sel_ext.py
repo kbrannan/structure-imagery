5d39f503f5c8c0c231c87f0ac2c55e997a8fe61a
kbrannan <brannan.kevin@deq.state.or.us>
2018-01-08 11:53:49 -0800

    created function that selects a feature and returns the extent of the selected feature as an extent object

--- get_sel_ext.py
+++ get_sel_ext.py
@@ -1,14 +1,16 @@
 def get_sel_extent(mxd, lyr, df, sqlquery):
-    str_path_mxd = r'\\deqhq1\tmdl\tmdl_wr\midcoast\GIS\BacteriaTMDL\UpperYaquinaRiver\MapDocs'
-    str_file_mxd = r'compare-near-streamstructures-locations-to-imagery-template20171227.mxd'
-    str_strc_cent = r'PointPotentialStructureCentroids'
-
-    from arcpy import mapping as MAP
     from arcpy import SelectLayerByAttribute_management as SEL_LYR
-    mxd_cur = MAP.MapDocument(str_path_mxd + "\\" + str_file_mxd)
-    df = MAP.ListDataFrames(mxd_cur)[0]
-    SelLayer = MAP.ListLayers(mxd, lyr, df)[0]
-    sqlquery = '"FID" = {}'.format(mylist[0])
-    SEL_LYR(SelLayer, 'NEW_SELECTION', sqlquery)
-    ext = SelLayer.getSelectedExtent()
+    SEL_LYR(lyr, 'NEW_SELECTION', sqlquery)
+    ext = lyr.getSelectedExtent()
     return ext
+
+
+from arcpy import mapping as MAP
+str_path_mxd = r'\\deqhq1\tmdl\tmdl_wr\midcoast\GIS\BacteriaTMDL\UpperYaquinaRiver\MapDocs'
+str_file_mxd = r'compare-near-streamstructures-locations-to-imagery-template20171227.mxd'
+str_strc_cent = r'PointPotentialStructureCentroids'
+str_sql = '"FID" = 5'
+mxd_cur = MAP.MapDocument(str_path_mxd + "\\" + str_file_mxd)
+df = MAP.ListDataFrames(mxd_cur)[0]
+lyr = MAP.ListLayers(mxd_cur, str_strc_cent, df)[0]
+cur_ext = get_sel_extent(mxd_cur,lyr,df,str_sql)
