7db5579b7caf4ed8acee811bb6a1aa0f5dd3210d
kbrannan <brannan.kevin@deq.state.or.us>
2018-01-10 13:03:02 -0800

    changed the mxd to have a standard page loayout for map. Also had to change some of the code to export the map using the page layout

--- zoom_to_feature.py
+++ zoom_to_feature.py
@@ -10,7 +10,7 @@ def get_sel_extent(lyr, sqlquery):
     return ext
 
 str_path_mxd = r'\\deqhq1\tmdl\tmdl_wr\midcoast\GIS\BacteriaTMDL\UpperYaquinaRiver\MapDocs'
-str_file_mxd = r'compare-near-streamstructures-locations-to-imagery-template20171227.mxd'
+str_file_mxd = r'Upper Yaquina Near-Stream Structures (scratch).mxd'
 str_strc_cent = r'PointPotentialStructureCentroids'
 
 str_path_export = r'\\deqhq1\tmdl\tmdl_wr\midcoast\GIS\BacteriaTMDL\UpperYaquinaRiver\python\structure-imagery\images'
@@ -18,7 +18,13 @@ str_file_image_export_prefix = 'strt_'
 
 if os.path.isfile(str_path_mxd + "\\" + str_file_mxd):
     mxd_cur = arcpy.mapping.MapDocument(str_path_mxd + "\\" + str_file_mxd)
+    mxd_cur.activeView='PAGE_LAYOUT' # make sure the page layout is current view to get map with mulp data frames
+    mxd_cur.save() # have to save and then reload mxd for the change in active view to take effect
+    del mxd_cur
+    mxd_cur = arcpy.mapping.MapDocument(str_path_mxd + "\\" + str_file_mxd)
     df = arcpy.mapping.ListDataFrames(mxd_cur)[0]
+
+
 
 SelLayer = arcpy.mapping.ListLayers(mxd_cur, str_strc_cent, df)[0]
 
@@ -32,6 +38,7 @@ for curFID in mylist:
     arcpy.mapping.ExportToPNG(mxd_cur, str_path_export + '\\' + str_file_image_export_prefix +
                               '{}'.format(curFID) + 'ext.png',df,
                               df_export_width=1600, df_export_height=1600, world_file=True)
+    arcpy.mapping.ExportToPNG(map_document=mxd_cur, out_png=str_path_export + '\\' + str_file_image_export_prefix +'{}'.format(curFID) + 'ext.png', df_export_width=1600, df_export_height=1600, world_file=True)
     del ext_cur
 
 
