2d08f230fbe9c8ac9a870ca5538c324f5916a15a
kbrannan <brannan.kevin@deq.state.or.us>
2018-01-10 13:32:52 -0800

    added code to make sure no features selected and using the correct data frame for layer

--- zoom_to_feature.py
+++ zoom_to_feature.py
@@ -5,12 +5,14 @@ def unique_values(table , field):
         return sorted({row[0] for row in cursor})
 
 def get_sel_extent(lyr, sqlquery):
+    arcpy.SelectLayerByAttribute_management(lyr, 'CLEAR_SELECTION')
     arcpy.SelectLayerByAttribute_management(lyr, 'NEW_SELECTION', sqlquery)
     ext = lyr.getSelectedExtent()
     return ext
 
 str_path_mxd = r'\\deqhq1\tmdl\tmdl_wr\midcoast\GIS\BacteriaTMDL\UpperYaquinaRiver\MapDocs'
 str_file_mxd = r'Upper Yaquina Near-Stream Structures (scratch).mxd'
+str_df_zoom_name = r'Zoom to Feature'
 str_strc_cent = r'PointPotentialStructureCentroids'
 
 str_path_export = r'\\deqhq1\tmdl\tmdl_wr\midcoast\GIS\BacteriaTMDL\UpperYaquinaRiver\python\structure-imagery\images'
@@ -18,15 +20,15 @@ str_file_image_export_prefix = 'strt_'
 
 if os.path.isfile(str_path_mxd + "\\" + str_file_mxd):
     mxd_cur = arcpy.mapping.MapDocument(str_path_mxd + "\\" + str_file_mxd)
+    df = arcpy.mapping.ListDataFrames(mxd_cur, str_df_zoom_name)[0]
     mxd_cur.activeView='PAGE_LAYOUT' # make sure the page layout is current view to get map with mulp data frames
     mxd_cur.save() # have to save and then reload mxd for the change in active view to take effect
     del mxd_cur
     mxd_cur = arcpy.mapping.MapDocument(str_path_mxd + "\\" + str_file_mxd)
-    df = arcpy.mapping.ListDataFrames(mxd_cur)[0]
-
 
 
 SelLayer = arcpy.mapping.ListLayers(mxd_cur, str_strc_cent, df)[0]
+arcpy.SelectLayerByAttribute_management(SelLayer, 'CLEAR_SELECTION')
 
 mylist = unique_values(SelLayer,'FID')
 
@@ -35,10 +37,9 @@ for curFID in mylist:
     ext_cur = get_sel_extent(SelLayer, query)
     df.panToExtent(ext_cur)
     arcpy.RefreshActiveView()
-    arcpy.mapping.ExportToPNG(mxd_cur, str_path_export + '\\' + str_file_image_export_prefix +
-                              '{}'.format(curFID) + 'ext.png',df,
-                              df_export_width=1600, df_export_height=1600, world_file=True)
-    arcpy.mapping.ExportToPNG(map_document=mxd_cur, out_png=str_path_export + '\\' + str_file_image_export_prefix +'{}'.format(curFID) + 'ext.png', df_export_width=1600, df_export_height=1600, world_file=True)
+    arcpy.mapping.ExportToPNG(map_document=mxd_cur, out_png=str_path_export + '\\' + str_file_image_export_prefix +'{}'.format(curFID) + '_ext_pg.png')
+    arcpy.mapping.ExportToPNG(map_document=mxd_cur, out_png=str_path_export + '\\' + str_file_image_export_prefix +'{}'.format(curFID) + '_ext_df.png',
+                              data_frame=df, df_export_height=1600, df_export_width=1600, world_file=True)
     del ext_cur
 
 
