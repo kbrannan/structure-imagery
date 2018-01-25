0b7166eabe6df6227902b710144695b5843d2e4a
kbrannan <brannan.kevin@deq.state.or.us>
2018-01-25 15:27:53 -0800

    adding specfic variables

--- zoom_to_feature.py
+++ zoom_to_feature.py
@@ -10,7 +10,7 @@ str_df_zoom_name = r'Zoom to Feature'
 str_df_state_name = r'Overall Watershed'
 str_strc_cent = r'PointPotentialStructureCentroids'
 str_strc_poly = r'Potential Structures'
-str_strm_lines = r'NHD Flowlines'
+str_strm_line = r'NHD Flowlines'
 
 str_path_export = r'\\deqhq1\tmdl\tmdl_wr\midcoast\GIS\BacteriaTMDL\UpperYaquinaRiver\python\structure-imagery\images'
 str_file_image_export_prefix = 'strt_'
@@ -19,19 +19,19 @@ memSelLyr = "in_memory" + "\\" + "memSelLayer"
 
 if os.path.isfile(str_path_mxd + "\\" + str_file_mxd):
     mxd_cur = arcpy.mapping.MapDocument(str_path_mxd + "\\" + str_file_mxd)
-    df.zoom = arcpy.mapping.ListDataFrames(mxd_cur, str_df_zoom_name)[0]
-    df.state = arcpy.mapping.ListDataFrames(mxd_cur, str_df_state_name)[0]
+    df_zoom = arcpy.mapping.ListDataFrames(mxd_cur, str_df_zoom_name)[0]
+    df_state = arcpy.mapping.ListDataFrames(mxd_cur, str_df_state_name)[0]
     #mxd_cur.activeView='PAGE_LAYOUT' # make sure the page layout is current view to get map with mulp data frames
     #mxd_cur.save() # have to save and then reload mxd for the change in active view to take effect
     #del mxd_cur
     #mxd_cur = arcpy.mapping.MapDocument(str_path_mxd + "\\" + str_file_mxd)
 
-SelLayer = arcpy.mapping.ListLayers(mxd_cur, str_strc_poly, df)[0]
+SelLayer = arcpy.mapping.ListLayers(mxd_cur, str_strc_poly, df_zoom)[0]
 
 mylist = unique_values(SelLayer,'FID')
 
 # make all layers not visible
-for lyr in df:
+for lyr in df_zoom:
     if lyr.isGroupLayer == True:
         #print "Layers in " + lyr.name
         lyr_g = arcpy.mapping.ListLayers(lyr)
@@ -43,7 +43,7 @@ for lyr in df:
     else:
         print lyr.name
         lyr.visble = False
-lyr = arcpy.mapping.ListLayers(mxd_cur, str_strm_line, df)[0]
+lyr = arcpy.mapping.ListLayers(mxd_cur, str_strm_line, df_zoom)[0]
 lyr.visible = True
 del lyr
 arcpy.RefreshTOC()
@@ -53,7 +53,7 @@ for curFID in mylist:
     # Process: Select
     arcpy.Select_analysis(SelLayer, memSelLyr, query)
     add_lyr = arcpy.mapping.Layer(memSelLyr)
-    arcpy.mapping.AddLayer(df, add_lyr, "TOP")
+    arcpy.mapping.AddLayer(df_zoom, add_lyr, "TOP")
     arcpy.SelectLayerByAttribute_management(in_layer_or_view=add_lyr, selection_type='NEW_SELECTION', where_clause=query)
     df.panToExtent(add_lyr.getSelectedExtent())
     #df.zoomToSelectedFeatures()
