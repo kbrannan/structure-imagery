45d08810f1e4a10ed2f552b70814a2a9ba81ef6b
kbrannan <brannan.kevin@deq.state.or.us>
2018-01-25 16:05:21 -0800

    moving functions to modeles

--- structureimageryModule/get_mxd.py
+++ structureimageryModule/get_mxd.py
@@ -3,4 +3,7 @@ from arcpy import mapping
 
 def get_mxd(str_path_mxd, str_file_mxd):
     if path.isfile(str_path_mxd + "\\" + str_file_mxd):
-        mxd_cur = mapping.MapDocument(str_path_mxd + "\\" + str_file_mxd)
+        mxd = mapping.MapDocument(str_path_mxd + "\\" + str_file_mxd)
+    else:
+        mxd = "can't find file " + str_file_mxd + " in folder " + str_path_mxd
+    return mxd
