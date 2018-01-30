55d1ccbd311a3cfe40954a3a6ace7cbd41421649
kbrannan <brannan.kevin@deq.state.or.us>
2018-01-30 15:10:44 -0800

    had an errant arcpy in the code

--- structureimageryModule/lyr_not_vis.py
+++ structureimageryModule/lyr_not_vis.py
@@ -4,11 +4,11 @@ from arcpy import RefreshTOC
 def lyr_not_vis(df_zoom):
     for lyr in df_zoom:
         if lyr.isGroupLayer == True:
-            lyr_g = arcpy.mapping.ListLayers(lyr)
+            lyr_g = mapping.ListLayers(lyr)
             for lyr_in_g in lyr_g:
                 lyr_in_g.visible = False
                 del lyr_in_g, lyr_g
         else:
             lyr.visble = False
         del lyr
-    arcpy.RefreshTOC()
+    RefreshTOC()
