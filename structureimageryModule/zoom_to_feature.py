9161fc771c3356abe9875d6f80f00dee7920703e
kbrannan <brannan.kevin@deq.state.or.us>
2018-01-30 15:14:51 -0800

    removed layer properties from the import command

--- structureimageryModule/zoom_to_feature.py
+++ structureimageryModule/zoom_to_feature.py
@@ -1,9 +1,7 @@
-from arcpy import panToExtent, getSelectedExtent, RefreshTOC, RefreshActiveView
-def zoom_to_feature(df_zoom, add_lyr)
+from arcpy import RefreshTOC, RefreshActiveView
+
+def zoom_to_feature(df_zoom, add_lyr):
     df_zoom.panToExtent(add_lyr.getSelectedExtent())
     add_lyr.visible = True
     RefreshTOC()
     RefreshActiveView()
-
-
-
