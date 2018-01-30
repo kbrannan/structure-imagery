13f652b08487c7f4d4d17c169cdaf51329f5146b
kbrannan <brannan.kevin@deq.state.or.us>
2018-01-30 15:16:13 -0800

    moved arcpy code to import

--- structureimageryModule/gen_map_images.py
+++ structureimageryModule/gen_map_images.py
@@ -5,11 +5,11 @@ def gen_map_images(mylist, SelLayer, df_zoom, mxd_cur, str_path_export, str_file
     memSelLyr = "in_memory" + "\\" + "memSelLayer"
     for curFID in mylist:
         query = '"FID" = {}'.format(curFID)
-        arcpy.Select_analysis(SelLayer, memSelLyr, query)
-        add_lyr = arcpy.mapping.Layer(memSelLyr)
-        arcpy.mapping.AddLayer(df_zoom, add_lyr, "TOP")
-        arcpy.SelectLayerByAttribute_management(in_layer_or_view=add_lyr, selection_type='NEW_SELECTION', where_clause=query)
-        arcpy.mapping.ExportToPNG(map_document=mxd_cur, out_png=str_path_export + '\\' + str_file_image_export_prefix +'{}'.format(curFID) + '_ext_pg.png')
-        arcpy.Delete_management(add_lyr)
-        arcpy.Delete_management("in_memory")
+        Select_analysis(SelLayer, memSelLyr, query)
+        add_lyr = mapping.Layer(memSelLyr)
+        mapping.AddLayer(df_zoom, add_lyr, "TOP")
+        SelectLayerByAttribute_management(in_layer_or_view=add_lyr, selection_type='NEW_SELECTION', where_clause=query)
+        mapping.ExportToPNG(map_document=mxd_cur, out_png=str_path_export + '\\' + str_file_image_export_prefix +'{}'.format(curFID) + '_ext_pg.png')
+        Delete_management(add_lyr)
+        Delete_management("in_memory")
         del query
