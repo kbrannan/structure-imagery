f9b53a92917f68c3f88da62d6d39653d8b2d1fc1
kbrannan <brannan.kevin@deq.state.or.us>
2018-01-30 16:51:20 -0800

    cant get function to work. get error with the select_analysis function. Says the sellayer in not a feature layer (it is). The same commands work in the zoom_to_feature_hold.py script

--- structureimageryModule/gen_map_images.py
+++ structureimageryModule/gen_map_images.py
@@ -1,15 +1,16 @@
-from arcpy import mapping
-from arcpy import Select_analysis, SelectLayerByAttribute_management, Delete_management
+# from arcpy import mapping
+# from arcpy import Select_analysis, SelectLayerByAttribute_management, Delete_management
+import arcpy
 
-def gen_map_images(mylist, SelLayer, df_zoom, mxd_cur, str_path_export, str_file_image_export_prefix):
+def gen_map_images(mylist, sellayer, df_zoom, mxd_cur, str_path_export, str_file_image_export_prefix):
     memSelLyr = "in_memory" + "\\" + "memSelLayer"
     for curFID in mylist:
         query = '"FID" = {}'.format(curFID)
-        Select_analysis(SelLayer, memSelLyr, query)
-        add_lyr = mapping.Layer(memSelLyr)
-        mapping.AddLayer(df_zoom, add_lyr, "TOP")
-        SelectLayerByAttribute_management(in_layer_or_view=add_lyr, selection_type='NEW_SELECTION', where_clause=query)
-        mapping.ExportToPNG(map_document=mxd_cur, out_png=str_path_export + '\\' + str_file_image_export_prefix +'{}'.format(curFID) + '_ext_pg.png')
-        Delete_management(add_lyr)
-        Delete_management("in_memory")
+        arcpy.Select_analysis(sellayer, memSelLyr, query)
+        add_lyr = arcpy.mapping.Layer(memSelLyr)
+        arcpy.mapping.AddLayer(df_zoom, add_lyr, "TOP")
+        arcpy.SelectLayerByAttribute_management(in_layer_or_view=add_lyr, selection_type='NEW_SELECTION', where_clause=query)
+        arcpy.mapping.ExportToPNG(map_document=mxd_cur, out_png=str_path_export + '\\' + str_file_image_export_prefix +'{}'.format(curFID) + '_ext_pg.png')
+        arcpy.Delete_management(add_lyr)
+        arcpy.Delete_management("in_memory")
         del query
