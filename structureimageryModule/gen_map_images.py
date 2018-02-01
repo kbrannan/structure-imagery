ff3471ab7040e5a81acdc7e05047ae2e25643618
kbrannan <brannan.kevin@deq.state.or.us>
2018-01-31 16:28:04 -0800

    renamed sellayr back to SelLayer like it is used in the script, function still doesnt work

--- structureimageryModule/gen_map_images.py
+++ structureimageryModule/gen_map_images.py
@@ -2,11 +2,11 @@
 # from arcpy import Select_analysis, SelectLayerByAttribute_management, Delete_management
 import arcpy
 
-def gen_map_images(mylist, sellayer, df_zoom, mxd_cur, str_path_export, str_file_image_export_prefix):
+def gen_map_images(mylist, SelLayer, df_zoom, mxd_cur, str_path_export, str_file_image_export_prefix):
     memSelLyr = "in_memory" + "\\" + "memSelLayer"
     for curFID in mylist:
         query = '"FID" = {}'.format(curFID)
-        arcpy.Select_analysis(sellayer, memSelLyr, query)
+        arcpy.Select_analysis(SelLayer, memSelLyr, query)
         add_lyr = arcpy.mapping.Layer(memSelLyr)
         arcpy.mapping.AddLayer(df_zoom, add_lyr, "TOP")
         arcpy.SelectLayerByAttribute_management(in_layer_or_view=add_lyr, selection_type='NEW_SELECTION', where_clause=query)
