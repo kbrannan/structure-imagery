ae838e890c3330483bae595b94e6dc42b5163444
BTDangelo <bernadette.dangelo@state.or.us>
2018-03-06 15:14:01 -0800

    updated to work with structureimagery script

--- single_map_script.py
+++ single_map_script.py
@@ -1,30 +1,32 @@
 import structureimagery as strimage
 
+#BRC placeholder
 
 str_path_mxd = r'\\deqhq1\tmdl\tmdl_wr\midcoast\GIS\BacteriaTMDL\UpperYaquinaRiver\MapDocs'
-str_file_mxd = r'UY_NSS_scratch.mxd'
+str_file_mxd = r'Upper Yaquina Near-Stream Structures (scratch).mxd'
 str_df_zoom_name = r'Zoom to Feature'
 str_df_state_name = r'Overall Watershed'
 str_strc_cent = r'PointPotentialStructureCentroids'
 str_strc_poly = r'Potential Structures'
 str_strm_line = r'NHD Flowlines'
-str_mxd_save_post = r".mxd"                     #BRC added
+ortho_bm = r'ortho_1-1_1n_s_or041_2014_1.sid'
 
 str_path_export = r'\\deqhq1\tmdl\tmdl_wr\midcoast\GIS\BacteriaTMDL\UpperYaquinaRiver\python\structure-imagery\images'
 str_file_image_export_prefix = 'strt_'
-mxd = (str_path_mxd + "\\" + str_file_mxd)  #BRC added (simplify the process?)
-mxd_cur = arcpy.mapping.MapDocument(mxd)     #BRC added
+
 curFID = 36
 
 
-#mxd_cur = strimage.get_mxd(str_path_mxd, str_file_mxd)     #BRC simplified getting .mxd
+mxd_cur = strimage.get_mxd(str_path_mxd, str_file_mxd)
 
 df_zoom = strimage.get_df(mxd_cur, str_df_zoom_name)
 
 sel_lyr = strimage.get_sel_layer(mxd_cur, str_strc_cent, df_zoom)
 
-#strimage.make_not_vis(df_zoom)
+strimage.make_not_vis(df_zoom)
 
-strimage.make_vis(mxd_cur, df_zoom, [str_strc_poly, str_strm_line])
+strimage.make_vis(mxd_cur, df_zoom, [str_strc_poly, str_strm_line, ortho_bm])
+
+# arcpy.mapping.MoveLayer(df_zoom, str_strc_cent, str_strc_poly, "BEFORE")
 
 strimage.gen_map_image(curFID, sel_lyr, df_zoom, mxd_cur, str_path_export, str_file_image_export_prefix)
